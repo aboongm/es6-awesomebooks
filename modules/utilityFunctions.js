d852308b2be920f5c339bff978f011ed95b47f09
aboongm <aboongm@yahoo.com>
2022-03-28 13:41:37 +0530

    Add removeBook and checkLocalStorage functions to utilityFunctions and export them

--- modules/utilityFunctions.js
+++ modules/utilityFunctions.js
@@ -1,6 +1,13 @@
 import books from './Books.js';
 import * as Elements from './constElements.js';
 
+function checkLocalStorage() {
+  if (JSON.parse(localStorage.getItem('BOOKS_LIST')) != null) {
+    books.BooksObject = JSON.parse(localStorage.getItem('BOOKS_LIST'));
+    createContent();
+  }
+}
+
 const addBook = () => {
   books.add(Elements.book, Elements.author);
   checkLocalStorage();
@@ -8,3 +15,9 @@ const addBook = () => {
   Elements.author.parentElement.parentElement.classList.add('hide');
   Elements.contactInfo.classList.add('hide');
 };
+
+const removeBook = (element) => {
+  books.remove(element);
+};
+
+export { addBook, removeBook, checkLocalStorage };
